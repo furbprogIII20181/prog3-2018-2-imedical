0231397404532ff5ae0c66dea7852d6e922c774e
gustavomerini <gustavomerini@hotmail.com>
2018-09-17 21:07:27 -0300

    adicionando angular material

--- imedical-frontend/src/app/app.module.ts
+++ imedical-frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { UserModule } from './user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
