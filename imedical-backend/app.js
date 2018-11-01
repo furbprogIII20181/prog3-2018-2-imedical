6a4656ae1b647302fe0e550830983aff73f17929
gustavomerini <gustavomerini@hotmail.com>
2018-11-01 19:54:17 -0300

    correção CORS error

--- imedical-backend/app.js
+++ imedical-backend/app.js
@@ -7,6 +7,19 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
+  next();
+});
+
 // Require our routes into the application.
 require("./server/routes")(app);
 app.get("*", (req, res) =>
